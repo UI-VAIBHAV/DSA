d1b7cdc61cc20582da65d10e50acfdb3da3d493b
UI-VAIBHAV <vaibhavpandey9125@gmail.com>
2026-03-14 00:35:52 +0530

    Time: 197 ms (66.55%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -0,0 +1,6 @@
+-- Write your PostgreSQL query statement below
+select employee_id from employees
+where salary<30000 and manager_id NOT in(
+    select employee_id from employees
+)
+order by employee_id;
