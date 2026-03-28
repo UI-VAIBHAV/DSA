4fcbff0dfac63f0bd5af5c534fe15d58813b88f1
UI-VAIBHAV <vaibhavpandey9125@gmail.com>
2026-03-28 23:39:04 +0530

    Time: 653 ms (24.33%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -0,0 +1,12 @@
+SELECT p.product_id,
+       COALESCE(
+           (
+               SELECT new_price
+               FROM products p2
+               WHERE p2.product_id = p.product_id
+                 AND p2.change_date <= '2019-08-16'
+               ORDER BY change_date DESC
+               LIMIT 1
+           ), 10
+       ) AS price
+FROM (SELECT DISTINCT product_id FROM products) p;
