d6d65e06680b0ae96e9ccabb872fddbdedb8424f
UI-VAIBHAV <vaibhavpandey9125@gmail.com>
2026-05-03 22:54:33 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (20.55%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int count(vector<int>&nums,vector<int>&dp,int idx){
+        if(idx>=nums.size()) return 0;
+        if(dp[idx]!=-1) return dp[idx];
+        int take = nums[idx] + count(nums,dp,idx+2);
+        int skip = count(nums,dp,idx+1);
+        return dp[idx] = max(take,skip);
+    }
+    int rob(vector<int>& nums) {
+        vector<int>dp(nums.size(),-1);
+        return count(nums,dp,0);
+    }
+};
