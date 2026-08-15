cf1b4721c8f136c0e8d758a2481882498721eb5a
Vaibhav Pandey <vaibhavpandey9125@gmail.com>
2026-08-16 01:34:35 +0530

    Time: 400 ms (23.67%) | Memory: 187.9 MB (5.08%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
-    int assign(vector<int>&nums,int target,int sum,int idx){
-        if(idx==nums.size()){
-            if(target==sum) return 1;
-            return 0;
-        }
-        int plus = assign(nums,target,sum+nums[idx],idx+1);
-        int minus = assign(nums,target,sum-nums[idx],idx+1);
-        return plus+minus;
+    int count(vector<int>nums,int target,int sum,int idx,vector<vector<int>>&dp,int num){
+        if(idx==nums.size()) return sum==target;
+        if(dp[idx][sum+num]!=-1) return dp[idx][sum+num];
+        int plus = count(nums,target,sum+nums[idx],idx+1,dp,num);
+        int minus = count(nums,target,sum-nums[idx],idx+1,dp,num);
+        return dp[idx][sum+num] = plus+minus;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        int sum=0;
-        return assign(nums,target,sum,0);
+        int sum=0; 
+        int num=0;
+        for(int i:nums) num += i;
+        vector<vector<int>>dp(nums.size()+1,vector<int>(2*num+1,-1));
+        return count(nums,target,sum,0,dp,num);
     }
 };
